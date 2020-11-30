d5f3096f423699d687e1183213e5850d2d1d758d
Ростислав Ермаченков <errosgen@yandex.ru>
2020-11-30 21:15:08 +0300

    Card view added

--- VKNewsfeed/Newsfeed/Cell/NewsfeedCell.swift
+++ VKNewsfeed/Newsfeed/Cell/NewsfeedCell.swift
@@ -30,6 +30,7 @@ protocol FeedCellPhotoAttachmentViewModel {
 class NewsfeedCell: UITableViewCell {
     
     static let reuseId = "NewsfeedCell"
+    @IBOutlet weak var cardView: UIView!
     
     @IBOutlet weak var iconImageView: WebImageView!
     @IBOutlet weak var namelabel: UILabel!
@@ -46,6 +47,12 @@ class NewsfeedCell: UITableViewCell {
         
         iconImageView.layer.cornerRadius = iconImageView.frame.width / 2
         iconImageView.clipsToBounds = true
+        
+        cardView.layer.cornerRadius = 10
+        cardView.clipsToBounds = true
+        
+        backgroundColor = .clear
+        selectionStyle = .none
     }
     
     func set(viewModel: FeedCellViewModel) {
