f42ff0cefbdb8d07ebb4122e25a6e865701566ad
Ростислав Ермаченков <errosgen@yandex.ru>
2020-12-30 22:54:17 +0300

    CircleProgressView added on auth scene

--- VKNewsfeed/AuthScene/AuthViewController.swift
+++ VKNewsfeed/AuthScene/AuthViewController.swift
@@ -11,16 +11,45 @@ class AuthViewController: UIViewController {
 
     private var authService: AuthService!
     
-    @IBAction func signInTouch(_ sender: UIButton) {
-        authService.wakeUpSession()
-    }
+    private var circleProgressView: CircleProgressView!
+    private var authorizeButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         authService = SceneDelegate.shared().authService
+        
+        circleProgressView = CircleProgressView(frame: CGRect(x: 160, y: 150, width: 220, height: 220))
+        circleProgressView.center = view.center
+        circleProgressView.backgroundColor = .white
+        view.addSubview(circleProgressView)
+        
+        authorizeButton = UIButton(type: .custom)
+        authorizeButton.frame = CGRect(x: 160, y: 150, width: 200, height: 200)
+        authorizeButton.backgroundColor = #colorLiteral(red: 0.9253895879, green: 0.9255481362, blue: 0.9253795743, alpha: 1)
+        authorizeButton.layer.cornerRadius = 0.5 * authorizeButton.bounds.size.width
+        authorizeButton.clipsToBounds = true
+        authorizeButton.setTitle("Войти в VK", for: .normal)
+        authorizeButton.titleLabel?.font = .boldSystemFont(ofSize: 24)
+        authorizeButton.setTitleColor(#colorLiteral(red: 0, green: 0.462745098, blue: 1, alpha: 1), for: .normal)
+        authorizeButton.addTarget(self, action: #selector(authorizeVK), for: .touchDown)
+        authorizeButton.addTarget(self, action: #selector(setCircleProgressAtZero), for: .touchUpInside)
+        authorizeButton.center = view.center
+        
+        view.addSubview(authorizeButton)
+        
     }
-
-
+    
+    @objc func authorizeVK() {
+        authorizeButton.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+        self.circleProgressView?.setProgress(newProgress: 1.0, animated: true) {
+            self.authService.wakeUpSession()
+        }
+    }
+    
+    @objc func setCircleProgressAtZero() {
+        authorizeButton.backgroundColor = #colorLiteral(red: 0.9253895879, green: 0.9255481362, blue: 0.9253795743, alpha: 1)
+        self.circleProgressView?.setProgress(newProgress: 0.0, animated: true)
+    }
 }
 
